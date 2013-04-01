67d0c4a3ad49de9a64583f9ad988416d98baf16e
Morlok8k <otis.spankmeyer@gmail.com>
2013-04-01 04:56:19 -0700

    Minor updates

--- graph.sh
+++ graph.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+echo Clannad_VN_Flowchart.dot
 dot -Tjpg Clannad_VN_Flowchart.dot -o Clannad_VN_Flowchart.jpg
+
+echo Clannad_VN_Flowchart_AfterStory.dot
 dot -Tjpg Clannad_VN_Flowchart_AfterStory.dot -o Clannad_VN_Flowchart_AfterStory.jpg
+
+echo Clannad_VN_Flowchart_Doki_Doki_HS.dot
 dot -Tjpg Clannad_VN_Flowchart_Doki_Doki_HS.dot -o Clannad_VN_Flowchart_Doki_Doki_HS.jpg
+
+echo Tomoyo_After_VN_Flowchart.dot
 dot -Tjpg Tomoyo_After_VN_Flowchart.dot -o Tomoyo_After_VN_Flowchart.jpg
