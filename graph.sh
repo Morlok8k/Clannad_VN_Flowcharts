ba7b01bd860a77d1b6d96c9e6bbd81fed6b64ca8
Morlok8k <otis.spankmeyer@gmail.com>
2013-04-08 15:08:32 -0700

    restructured the file tree, made changes to scripts

--- graph.sh
+++ graph.sh
@@ -27,16 +27,16 @@
 
 echo -- Graphing:
 echo -- Clannad_VN_Flowchart.dot
-dot -Tpng Clannad_VN_Flowchart.dot -o Clannad_VN_Flowchart.png
+dot -Tpng ./dot_files/Clannad_VN_Flowchart.dot -o ./Clannad_VN_Flowchart.png
 
 echo -- Clannad_VN_Flowchart_AfterStory.dot
-dot -Tpng Clannad_VN_Flowchart_AfterStory.dot -o Clannad_VN_Flowchart_AfterStory.png
+dot -Tpng ./dot_files/Clannad_VN_Flowchart_AfterStory.dot -o ./Clannad_VN_Flowchart_AfterStory.png
 
 echo -- Clannad_VN_Flowchart_Doki_Doki_HS.dot
-dot -Tpng Clannad_VN_Flowchart_Doki_Doki_HS.dot -o Clannad_VN_Flowchart_Doki_Doki_HS.png
+dot -Tpng ./dot_files/Clannad_VN_Flowchart_Doki_Doki_HS.dot -o ./Clannad_VN_Flowchart_Doki_Doki_HS.png
 
 echo -- Tomoyo_After_VN_Flowchart.dot
-dot -Tpng Tomoyo_After_VN_Flowchart.dot -o Tomoyo_After_VN_Flowchart.png
+dot -Tpng ./dot_files/Tomoyo_After_VN_Flowchart.dot -o ./Tomoyo_After_VN_Flowchart.png
 
 echo -- Done!
 
