a1ec8b5d04ec549d103c356b83f6ef05d9e71b37
Morlok8k <otis.spankmeyer@gmail.com>
2013-04-28 00:04:59 -0700

    minor updates to script

--- graph.sh
+++ graph.sh
@@ -33,7 +33,8 @@ for dot in `find $1 -name "*.dot"`;
 do
   echo "-- Graphing: $dot"
   dot -Tpng "$dot" -O     # creates ./dot_files/Clannad_VN_Flowchart.dot.png
-  
+
+  ## Filename string manipulation
   PART4=$dot              # PART4 is ./dot_files/Clannad_VN_Flowchart.dot
   PART1="${PART4%%/*}"    # PART1 is .
   PART4="${PART4#*/}"     # PART4 is /dot_files/Clannad_VN_Flowchart.dot
@@ -43,25 +44,32 @@ do
                           # PART4 is .dot
   PART4="${PART4#*.}"     # PART4 is dot
 
-  # Notice: The above fails if the filename starts with a "." for a hidden file.
+  # Notice: The above fails if the filename starts with a "." for a hidden file.  ex: "./dot_files/.temp.dot"
   #         Thus we have the following check:
   if [ "$PART3" = "" ]
   then
     PART3=".${PART4%%.*}"     #fixing
   fi
 
-  # move .png file down one folder, change *.dot.png to just *.png
+
+  #echo "-- Parsing: ./$PART2/$PART3_Parsed.txt"
+  #dot -Tdot "$dot" -o "$PART3_Parsed.txt"     # Creates a new .dot file stripped of comments, but with positions for everything...
+                                               # It will be useful once graphs are completed to make adjustments for final positioning.
+                                               # I give it a .txt extension so it wont be affected by this script when its run again...
+
+
+  ## move .png file down one folder, change *.dot.png to just *.png
   mv ./$PART2/$PART3.dot.png ./$PART3.png
 
   echo "-- Compressing: $PART3.png" 
   pngcrush -q -m 113 $PART3.png .temp_png.png
-  # pngcrush can complain about overwriting a file.  thus temp.png
+       # pngcrush can complain about overwriting a file.  thus .temp_png.png
   mv -f .temp_png.png $PART3.png
 
   echo " "
 
 done;
 
-echo - Done!
+echo "- Done!"
 
 
